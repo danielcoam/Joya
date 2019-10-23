efa23cb9e1f7a12988134a10547e934bcd86882e
rona arv <arvivro@gmail.com>
2019-10-23 17:21:20 +0300

    add google map

--- Data/JoyaDbContext.cs
+++ Data/JoyaDbContext.cs
@@ -27,5 +27,6 @@ namespace Joya.Data
         public DbSet<Customer> Customers { get; set; }
         public DbSet<Bracelet> Bracelets { get; set; }
         public DbSet<Order> Orders { get; set; }
+        public DbSet<Joya.Models.Store> Store { get; set; }
     }
 }
---
+++ Models/Store.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Joya.Models
+{
+    public class Store
+    {
+        public int Id{get; set;}
+        public string streetName { get; set; }
+        public decimal latitude { get; set; }
+        public decimal longitude { get; set; }
+    }
+}
